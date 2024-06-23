608aae23cf6754f272d8d402eb02bcebb6544d40
Aditya <adityadange135@gmail.com>
2024-06-23 15:28:13 +0530

    Date:- 23/6/2024 Goal complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 122222;
+let accountEmail = "Adi@google.com";
+var accountPassword = "12345";
+accountCity = "Pune"
+let accountState;
+
+// accountId = 133333;
+
+accountEmail = "Aditya@gmail.com"
+accountPassword = "67890"
+accountCity = "Benguluru"
+
+// console.log(accountId);
+/*
+Note: Please prefer do not use var, Because of Block scope and functional scope
+scope:{}
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(4+4); // We r using node.js not browser
+
+let name = "Aditya";
+let age = 20;
+let isLoggedIn = true;
+let state = null;
+
+// Number => age,ranges between 2^53
+// String => name, text,""
+// Boolean => true,false
+// null => standalone value ::typeof - object
+// undefined => not given value
+// symbol => unique
+
+// object
+
+console.log(typeof null); //
